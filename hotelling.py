b0ab8b4682b8b97c7c3ae92aa20156281e31a155
tsurubee <hirofumi.tsuruta.314@gmail.com>
2017-09-23 23:24:54 +0900

    Add module of outlier detection based on hotelling theory

---
+++ hotelling.py
@@ -0,0 +1,28 @@
+import numpy as np
+from scipy import stats
+
+def hotelling_1d(data, threshold):
+    """
+    Parameters
+        ----------
+        data : Numpy array
+        threshold : float
+
+    Returns
+    -------
+        List of tuples where each tuple contains index number and anomalous value.
+    """
+    #Set the threshold of abnormality
+    abn_th = stats.chi2.interval(1-threshold, 1)[1]
+
+    #Covert raw data into the degree of abnormality
+    avg = np.average(data)
+    var = np.var(data)
+    data_abn = [(x - avg)**2 / var for x in data]
+
+    #Abnormality determination
+    result = []
+    for (index, x) in enumerate(data_abn):
+        if x > abn_th:
+            result.append((index, data[index]))
+    return result
