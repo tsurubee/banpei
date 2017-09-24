e9af76959621174b5afcb54c9b627bc07a8517f3
tsurubee <hirofumi.tsuruta.314@gmail.com>
2017-09-24 22:12:36 +0900

    Change directory structure for packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='anomaly_detector',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+
+    ]
+)
