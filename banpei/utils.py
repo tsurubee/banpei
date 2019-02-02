e08bfb187d3d37de88841c9ffd3b7572cf9d3b0d
tsurubee <29603512+tsurubee@users.noreply.github.com>
2019-02-02 21:55:45 +0900

    Acceleration of calculation by Krylov subspace

--- banpei/utils.py
+++ banpei/utils.py
@@ -33,3 +33,116 @@ def power_method(A, iter_num=1):
     u = Av / s
 
     return u, s, v
+
+def tridiagonalize_by_lanczos(P, m, k):
+    """
+    Tridiagonalize matrix by lanczos method
+    Parameters
+    ----------
+    P : numpy array
+        Target matrix
+    q : numpy array
+        Initial vector
+    k : int
+        Size of the tridiagonal matrix
+
+    Returns
+    -------
+    T : numpy array
+        tridiagonal matrix
+    """
+    # Initialize variables
+    T = np.zeros((k, k))
+    r0 = m
+    beta0 = 1
+    q0 = np.zeros(m.shape)
+
+    for i in range(k):
+        q1 = r0 / beta0
+        C = np.dot(P, q1)
+        alpha1 = np.dot(q1, C)
+        r1 = C - alpha1 * q1 - beta0 * q0
+        beta1 = np.linalg.norm(r1)
+
+        T[i, i] = alpha1
+        if i + 1 < k:
+          T[i, i + 1] = beta1
+          T[i + 1, i] = beta1
+
+        q0 = q1
+        beta0 = beta1
+        r0 = r1
+
+    return T
+
+def tridiag_eigen(T, iter_num=1, tol=1e-3):
+    """
+    Calculate eigenvalues and eigenvectors of tridiagonal matrix
+    Parameters
+    ----------
+    P : numpy array
+        Target matrix (tridiagonal)
+    iter_num : int
+        Number of iterations
+    tol : float
+        Stop iteration if the target matrix converges to a diagonal matrix with acceptable tolerance `tol`
+
+    Returns
+    -------
+    eigenvalue : numpy array
+                 Calculated eigenvalues
+    eigenvectors : numpy array
+                 Calculated eigenvectors
+    """
+    eigenvectors = np.identity(T.shape[0])
+
+    for i in range(iter_num):
+        Q, R = tridiag_qr_decomposition(T)
+        T = np.dot(R, Q)
+        eigenvectors = np.dot(eigenvectors, Q)
+        eigenvalue = np.diag(T)
+        if np.all((T - np.diag(eigenvalue) < tol)):
+            break
+
+    return eigenvalue, eigenvectors
+
+def tridiag_qr_decomposition(T):
+    """
+    QR decomposition for a tridiagonal matrix
+    Ref. http://www.ericmart.in/blog/optimizing_julia_tridiag_qr
+    Parameters
+    ----------
+    T : numpy array
+        Target matrix (tridiagonal)
+
+    Returns
+    -------
+    Qt.T : numpy array
+    R    : numpy array
+    """
+    R = T.copy()
+    Qt = np.eye(T.shape[0])
+
+    for i in range(T.shape[0] - 1):
+        u = householder(R[i:i + 2, i])
+        M = np.outer(u, u)
+        R[i:i + 2, :(i + 3)] -= 2 * np.dot(M, R[i:i + 2, :(i + 3)])
+        Qt[i:i + 2, :(i + 3)] -= 2 * np.dot(M, Qt[i:i + 2, :(i + 3)])
+
+    return Qt.T, R
+
+def householder(x):
+    """
+    Householder projection for vector.
+    Parameters
+    ----------
+    x : numpy array
+        Target vector
+
+    Returns
+    -------
+    x : numpy array
+    """
+    x[0] = x[0] + np.sign(x[0]) * np.linalg.norm(x)
+    x = x / np.linalg.norm(x)
+    return x
